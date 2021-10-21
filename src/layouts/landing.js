18b25713ad12801e0df2221c05fdff80db0008ea
Stackbit Code Editor <projects@stackbit.com>
2021-10-21 21:21:17 +0000

    landing.js: changed

--- src/layouts/landing.js
+++ src/layouts/landing.js
@@ -27,19 +27,7 @@ export default class Landing extends React.Component {
                     }
                     return <Component key={index} section={section} data={data} posts={posts} />;
                 })}
-                <!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/6171bf58f7c0440a591f66a1/1fii56ssv';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
+                
             </Layout>
         );
     }
